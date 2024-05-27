61b59d5db3646c87b12f8ca5d6f2572594de9481
Picasso <79357222+GianSalvadora@users.noreply.github.com>
2024-05-27 19:07:03 +0800

    Added: -Basic landing page -Site intro animation Modified: -Nav bar to be partially visible before scrolling -More button now also turns yellow -Underline -Usage of global color variables

--- js/core.js
+++ js/core.js
@@ -44,3 +44,28 @@ function includeHTML() {
         }
     }
 }
+window.onload = function() {
+    var myDiv = document.getElementById('intro');
+    var delay = 5000; // 5 seconds
+
+    setTimeout(function() {
+        myDiv.style.display = 'none';
+        document.body.style.overflow = 'visible'
+    }, delay);
+};
+
+/* Changes the visibility of nav-bar background and border depending on the scroll value*/
+function navBackground(){
+    var navBar = document.getElementById('nav-container');
+    var scrollY = window.scrollY;
+
+    if(scrollY > navBar.getAttribute('height')){
+        navBar.classList.remove('hero-nav');
+    }
+    else{
+        navBar.classList.add(('hero-nav'));
+    }
+
+    console.log(scrollY);
+}
+window.addEventListener('scroll', navBackground);
