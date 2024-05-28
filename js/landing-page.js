db3b34c1ffbade8915bdb95e63d2a99a1ffacc73
Picasso <79357222+GianSalvadora@users.noreply.github.com>
2024-05-29 01:32:45 +0800

    Added: - Courses offered section (Incomplete) Fixed: - Blue highlighting on 'a' tag on nav bar Changed: -Moved all unnecessary landing-page styling to landing-page.css from core.css

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,7 +1,7 @@
 /* Handles the visibility of the intro animation and allows the user to scroll afterward */
 window.onload = function() {
-    var myDiv = document.getElementById('intro');
-    var delay = 5000; // 5 seconds
+    let myDiv = document.getElementById('intro');
+    let delay = 5000; // 5 seconds
 
     setTimeout(function() {
         myDiv.style.display = 'none';
@@ -11,8 +11,8 @@ window.onload = function() {
 
 /* Changes the visibility of nav-bar background and border depending on the scroll value*/
 function navBackground(){
-    var navBar = document.getElementById('nav-container');
-    var scrollY = window.scrollY;
+    let navBar = document.getElementById('nav-container');
+    let scrollY = window.scrollY;
 
     if(scrollY > navBar.getAttribute('height')){
         navBar.classList.remove('hero-nav');
@@ -24,3 +24,20 @@ function navBackground(){
     console.log(scrollY);
 }
 window.addEventListener('scroll', navBackground);
+
+
+document.getElementById('right-arrow').onclick = function (){
+    arrowClicked(1);
+}
+document.getElementById('left-arrow').onclick = function (){
+    arrowClicked(-1);
+}
+
+function arrowClicked(direction){
+    let card = document.getElementById('card');
+    card.style.animationName = 'card-fade';
+    card.addEventListener('animationend', function (){
+        card.style.animationName = 'none';
+        //I'll change the course data in this part
+    })
+}
